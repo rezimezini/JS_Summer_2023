14a02c1ad03f03a71818552bf2e46cd0b79dfee8
marselmezini <123007920+marselmezini@users.noreply.github.com>
2023-05-18 19:47:39 -0500

    Add files via upload

---
+++ Asignment1.js
@@ -0,0 +1,33 @@
+
+let myName = "Rezi";
+console.log(myName);
+
+let myAge = 40;
+console.log(myAge);
+
+let isStudent= true;
+console.log(isStudent);
+
+let colors= ["green","blu","red"];
+console.log(`\n my favorite colors are: ${colors}s`);
+
+
+console.log(`${typeof colors}`);
+
+let personalInfo = {
+    myName:"Rezi Mezini",
+    myAge: 40,
+    isStudent: true,
+    
+}
+
+console.log(`\n\n ${personalInfo}`);
+
+console.log(`${typeof personalInfo}`);
+
+console.log(personalInfo.myName);
+
+console.log(`${typeof personalInfo.isStudent}`);
+
+
+
